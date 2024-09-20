95475ea3f18e63eafbe124e2f3d2564ee87c7f3c
Collins Muriuki <hello@collinsmuriuki.xyz>
2024-09-20 03:23:59 +0300

    add extra plugins

--- config.lua
+++ config.lua
@@ -46,7 +46,59 @@ lvim.plugins = {
       })
     end,
   },
-  { "tpope/vim-fugitive" }
+  { "tpope/vim-fugitive" },
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+  },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = "nvim-lua/plenary.nvim",
+    config = function()
+      require("todo-comments").setup {}
+    end
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require('gitsigns').setup()
+    end
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+      require("indent_blankline").setup {}
+    end
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    config = function()
+      require("treesitter-context").setup {}
+    end
+  },
+  -- {
+  --   "folke/which-key.nvim",
+  --   config = function()
+  --     vim.o.timeout = true
+  --     vim.o.timeoutlen = 300
+  --     require("which-key").setup {}
+  --   end
+  -- },
+  {
+    "folke/trouble.nvim",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("trouble").setup {}
+    end
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({})
+    end
+  }
   --{
   --"andymass/vim-matchup",
   --event = "CursorMoved",
@@ -60,46 +112,44 @@ lvim.colorscheme = "gruvbox"
 
 lvim.format_on_save.enabled = true
 
--- keybinds
+-- general keybindings
 lvim.keys.normal_mode["<C-t>"] = ":pop<cr>"
 
--- vim fugitive keybinds
+-- vim fugitive keybindings
 lvim.keys.normal_mode["<leader>gs"] = ":Git<CR>"
 lvim.keys.normal_mode["<leader>gc"] = ":Git commit<CR>"
 lvim.keys.normal_mode["<leader>gp"] = ":Git push<CR>"
 
+-- Telescope keybindings
+lvim.keys.normal_mode["<leader>ff"] = ":Telescope find_files<CR>"
+lvim.keys.normal_mode["<leader>fg"] = ":Telescope live_grep<CR>"
+lvim.keys.normal_mode["<leader>fb"] = ":Telescope buffers<CR>"
+lvim.keys.normal_mode["<leader>fh"] = ":Telescope help_tags<CR>"
 lvim.builtin.treesitter.matchup.enable = true
 
--- prettier
--- local formatters = require "lvim.lsp.null-ls.formatters"
--- formatters.setup {
---   {
---     command = "prettier",
---     filetypes = {
---       "javascript",
---       "typescript",
---       "css",
---       "scss",
---       "html",
---       "json",
---       "yaml",
---       "markdown",
---       "graphql"
---     },
---   },
--- }
+-- Trouble keybindings
+lvim.keys.normal_mode["<leader>xx"] = "<cmd>TroubleToggle<cr>"
+lvim.keys.normal_mode["<leader>xw"] = "<cmd>TroubleToggle workspace_diagnostics<cr>"
+lvim.keys.normal_mode["<leader>xd"] = "<cmd>TroubleToggle document_diagnostics<cr>"
+lvim.keys.normal_mode["<leader>xl"] = "<cmd>TroubleToggle loclist<cr>"
+lvim.keys.normal_mode["<leader>xq"] = "<cmd>TroubleToggle quickfix<cr>"
+lvim.keys.normal_mode["gR"] = "<cmd>TroubleToggle lsp_references<cr>"
 
--- eslint
+-- prettier formatter
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
   {
-    command = "eslint",
+    command = "prettier",
     filetypes = {
       "javascript",
       "typescript",
-      "javascriptreact",
-      "typescriptreact",
-      "vue",
+      "css",
+      "scss",
+      "html",
+      "json",
+      "yaml",
+      "markdown",
+      "graphql"
     },
   },
 }
