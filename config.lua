8a891bae54e3c13c8283a9dfe404d4199a4297cf
Collins Muriuki <hello@collinsmuriuki.xyz>
2024-11-02 11:43:23 +0300

    Add vim.o.background setting

--- config.lua
+++ config.lua
@@ -5,6 +5,7 @@
 
 -- General settings
 lvim.colorscheme = "gruvbox"
+vim.o.background = "dark" -- "light" or "dark"
 lvim.format_on_save.enabled = true
 vim.opt.relativenumber = true
 -- enable auto-completion
